162fc6fd51a279f05266aef44711dd6b5efb16bd
timolai-andrievich <timolai.andrievitch@ya.ru>
2024-04-28 17:00:45 +0300

    Updated the report to include email and github

--- assignment3/report/compile.sh
+++ assignment3/report/compile.sh
@@ -4,4 +4,4 @@ bibtex report
 # Yes, the pdflatex needs to be run two times in a row
 pdflatex report.tex
 pdflatex report.tex
-rm report.aux  report.bbl  report.blg  report.log
+rm -f report.aux report.bbl report.blg report.log report.out
